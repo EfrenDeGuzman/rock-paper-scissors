0e7bc1535757241c177b7d73089c96fd73e40950
Efren <efrende@gmail.com>
2023-02-15 16:55:06 +0800

    Add playRound function to script.js

--- script.js
+++ script.js
@@ -7,8 +7,39 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "It's a Tie!"
+    }
+
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You Win! Rock beats Scissors"
+    }
     
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock"
+    }
+
+    if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock"
+    }
+    
+    if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You Lose! Scissors beats paper"
+    }
+
+    if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You Win! Scissors beats Paper"
+    }
+    
+    if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You Lose! Rock beats Paper"
+    }
+
 }
 
-console.log(getComputerChoice());
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
 
